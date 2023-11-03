8e6b081bbb48843a23f552ba30146a18ff4bec8f
Huy Quốc <huycon78@gmail.com>
2023-11-03 15:04:34 +0700

    loading

--- src/pages/location.jsx
+++ src/pages/location.jsx
@@ -75,6 +75,58 @@ const Title = styled.a`
     word-wrap: break-word;
 `;
 
+const mltShdSpin = keyframes`
+  0% {
+    box-shadow: 0 -0.83em 0 -0.4em,
+    0 -0.83em 0 -0.42em, 0 -0.83em 0 -0.44em,
+    0 -0.83em 0 -0.46em, 0 -0.83em 0 -0.477em;
+  }
+  5%,
+  95% {
+    box-shadow: 0 -0.83em 0 -0.4em, 
+    0 -0.83em 0 -0.42em, 0 -0.83em 0 -0.44em, 
+    0 -0.83em 0 -0.46em, 0 -0.83em 0 -0.477em;
+  }
+  10%,
+  59% {
+    box-shadow: 0 -0.83em 0 -0.4em, 
+    -0.087em -0.825em 0 -0.42em, -0.173em -0.812em 0 -0.44em, 
+    -0.256em -0.789em 0 -0.46em, -0.297em -0.775em 0 -0.477em;
+  }
+  20% {
+    box-shadow: 0 -0.83em 0 -0.4em, -0.338em -0.758em 0 -0.42em,
+     -0.555em -0.617em 0 -0.44em, -0.671em -0.488em 0 -0.46em, 
+     -0.749em -0.34em 0 -0.477em;
+  }
+  38% {
+    box-shadow: 0 -0.83em 0 -0.4em, -0.377em -0.74em 0 -0.42em,
+     -0.645em -0.522em 0 -0.44em, -0.775em -0.297em 0 -0.46em, 
+     -0.82em -0.09em 0 -0.477em;
+  }
+  100% {
+    box-shadow: 0 -0.83em 0 -0.4em, 0 -0.83em 0 -0.42em, 
+    0 -0.83em 0 -0.44em, 0 -0.83em 0 -0.46em, 0 -0.83em 0 -0.477em;
+  }
+`
+
+const round = keyframes`
+  0% { transform: rotate(0deg) }
+  100% { transform: rotate(360deg) }
+`
+
+const Loader = styled.span`
+  color: black;
+  font-size: 40px;
+  width: 1em;
+  height: 1em;
+  border-radius: 50%;
+  transform: translateZ(0);
+  margin: 20px;
+  animation: ${mltShdSpin} 1.7s infinite ease, ${round} 1.7s infinite ease;
+`
+
+
+
 const Location = () => {
     const dispatch = useDispatch();
     const [loading, setLoading] = useState(false);
@@ -86,8 +138,7 @@ const Location = () => {
         }
     }, [loading]);
 
-    useEffect(() =>{
-        console.log(`${process.env.REACT_APP_SERVER_URL}/questions/locations`);
+    useEffect(() => {
         fetch(`${process.env.REACT_APP_SERVER_URL}/questions/locations`, {
             method: "GET",
             headers: {
@@ -98,28 +149,40 @@ const Location = () => {
             .then((data) => {
                 setLocations(data.locations);
             });
-    },[]);
+    }, []);
 
     const handleClick = (location) => {
         dispatch(addLocation(location))
         setLoading(true);
         console.log(location)
-}
+    }
     return (
         <Container>
             <Wrapper>
-                <Title>
-                    Bạn vui lòng chọn cơ sở đã trải nghiệm:
-                </Title>
-                <Content>
-                        {locations.map((location, index) => {
-                            index += 1;
-                            return(
-                            <Button onClick={() => handleClick(location)} key={index}>
-                                {location}
-                            </Button>
-                        )})}
-                </Content>
+            {!locations ?
+                    <>
+                        <Loader />
+                        <Title>
+                            Xin vui lòng chờ đợi 
+                        </Title>
+                    </> 
+                    :
+                    <>
+                        <Title>
+                            Bạn vui lòng chọn cơ sở đã trải nghiệm:
+                        </Title>
+                        <Content>
+                            {locations.map((location, index) => {
+                                index += 1;
+                                return (
+                                    <Button onClick={() => handleClick(location)} key={index}>
+                                        {location}
+                                    </Button>
+                                )
+                            })}
+                        </Content>
+                    </>
+                }
             </Wrapper>
         </Container>
     );
